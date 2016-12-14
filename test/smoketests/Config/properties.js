45d93a2684f1d6d059d65dec2adece5cabb49a42
vivekr.dr <vivek.reddy@transformuk.com>
2016-12-14 16:57:43 +0000

    SSCS-509: Added files for supporting CI pipeline.
    New Jenkin files created for CI pipeline.
    "Jenkinsfile" file for running different stages: unit tests, accessibility
    and build.
    "Jenkinsfile-end-to-end.groovy" file for end to end tests.
    Updated package.json for supporting mock tests.
    See https://jenkins.io/doc/book/pipeline/jenkinsfile/ for more details
    about Jenkinsfiles.
    
    "test" folder created with mock smoke test.
    this smoke test runs on our prototype url and there is no real importance
    for this mock test,
    it is just created here for proving that we can integrate end to end tests
    as a part of CI pipeline.
    Note: these tests will be updated with valid testes in future.
    
    this smoke test runs on headless browser using using Codeceptjs and
    Nightmare framework.
    check http://codecept.io/helpers/Nightmare/ for more details on
    Codeceptjs.
    
    Files updated as suggested in branch merge request
    http://10.196.60.5/SSCS/track-your-appeal-frontend/merge_requests/2

---
+++ test/smoketests/Config/properties.js
@@ -0,0 +1,26 @@
+
+
+
+'use strict';
+
+let I;
+
+module.exports = {
+  _init() {
+    I = actor();
+  },
+
+  fields: {
+
+  //Track Appeal Page Properties
+      form_text_label_path:{css: '.form-label'},
+      form_hint_css_path:{css: '.form-hint'},
+      ref_css_path: 'input[name=reference]',
+      ref_id: '123/45/00001',
+      find_appeal_button: 'input[type=submit]',
+      Page_heading_css_path:{css: 'h1'}
+    }
+
+
+
+  };
