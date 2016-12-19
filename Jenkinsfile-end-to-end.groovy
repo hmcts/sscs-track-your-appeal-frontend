a5c41298a09507add91c63d53e2993313a9fa4c7
Rob Young <rob@robyoung.digital>
2016-12-19 11:25:01 +0000

    SSCS-509 Add editorconfig for Jenkinsfiles
    I was not able to find a definitive answer of what the accepted style is
    for Groovy files so I've chosen to stick as close to the existing files
    as possible.

--- Jenkinsfile-end-to-end.groovy
+++ Jenkinsfile-end-to-end.groovy
@@ -1,29 +1,29 @@
 node("Slave1") {
+    env.https_proxy = "http://172.16.200.254:3128/"
+    env.http_proxy = "http://172.16.200.254:3128/"
+    env.E2E_FRONTEND_URL = "http://www.sscs.sandbox.reform.ukc.hmcts.net/prototype-6"
+    env.E2E_OUTPUT_DIR = "${env.JENKINS_HOME}/testResults/"
+    env.npm_config_tmp = "${pwd()}/.tmp"
+
     stage "Run smoke tests"
-
     ws('workspace/track-your-appeal/smoke-tests') {
-
         git url: 'git@10.196.60.5:SSCS/track-your-appeal-frontend.git', branch: "$BRANCH"
-        env.https_proxy = "http://172.16.200.254:3128/"
-        env.http_proxy = "http://172.16.200.254:3128/"
         echo "Running Smoke Tests using Codeceptjs"
-        env.E2E_FRONTEND_URL = "http://www.sscs.sandbox.reform.ukc.hmcts.net/prototype-6"
-        env.E2E_OUTPUT_DIR = "${env.JENKINS_HOME}/testResults/"
-        env.npm_config_tmp = "${pwd()}/.tmp"
         wrap([$class: 'Xvfb']) {
             sh '''
-                                npm install
-                                npm run smoke-test -- --reporter mochawesome
-                            '''
+                npm install
+                npm run smoke-test -- --reporter mochawesome
+            '''
         }
     }
 
     publishHTML(target: [
-            alwaysLinkToLastBuild: true,
-            reportDir            : "${env.JENKINS_HOME}/testResults/",
-            reportFiles          : "mochawesome.html",
-            reportName           : "Functional Test Report"
+        alwaysLinkToLastBuild: true,
+        reportDir            : "${env.JENKINS_HOME}/testResults/",
+        reportFiles          : "mochawesome.html",
+        reportName           : "Functional Test Report"
     ])
+
     ws('workspace/track-your-appeal/smoke-tests') {
         deleteDir()
     }
