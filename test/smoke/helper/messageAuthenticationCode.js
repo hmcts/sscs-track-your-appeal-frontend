f5b9853a1cec347ed7b84c5310b5fc733ae3eff4
Rob Young <rob@robyoung.digital>
2017-03-20 17:39:45 +0000

    Refactor end to end test helpers
    Factor out an API helper that does not depend on codeceptjs. This
    reduces the complexity and duplication in the helpers and means that the
    same code can be reused outside of the context of codeceptjs.

--- test/smoke/helper/messageAuthenticationCode.js
+++ test/smoke/helper/messageAuthenticationCode.js
@@ -4,7 +4,8 @@ const secret_key = process.env.EMAIL_MAC_SECRET_TEXT || "our-big-secret",
   URLSafeBase64 = require('urlsafe-base64'),
   config = require('../props/postdatatoAPI'),
   request = require('superagent'),
-  assert = require('assert');
+  assert = require('assert'),
+  apiHelper = require('./apiHelper');
 
 
 class messageAuthenticationCodeHelper extends Helper {
@@ -12,76 +13,36 @@ class messageAuthenticationCodeHelper extends Helper {
     super(config);
     this.appeal_id = null;
     this.authentication_code = null;
+    this.apiHelper = new apiHelper.APIHelper();
   }
 
   getMACToken() {
-    return this.generateSubscriptionID().then((result, reject) => {
+    return this.generateSubscriptionID()
+      .then(result => {
+        const unixTimeStamp = Math.floor((new Date).getTime() / 1000);
+        const subscriptionId = result.subscriptionId;
 
-      const unixTimeStamp = Math.floor((new Date).getTime() / 1000);
-      const subscriptionId = result.id;
-      console.log("Subscription id is :" + subscriptionId);
+        let hmacEncription = cryptoJS.HmacSHA256(subscriptionId + "|" + unixTimeStamp, secret_key);
+        let hashInBase64 = cryptoJS.enc.Base64.stringify(hmacEncription).substring(0, 10);
+        let buffer = new Buffer(subscriptionId + "|" + unixTimeStamp + "|" + hashInBase64);
 
-      let hmacEncription = cryptoJS.HmacSHA256(subscriptionId + "|" + unixTimeStamp, secret_key);
-      let hashInBase64 = cryptoJS.enc.Base64.stringify(hmacEncription).substring(0, 10);
-      let buffer = new Buffer(subscriptionId + "|" + unixTimeStamp + "|" + hashInBase64);
+        this.authentication_code = URLSafeBase64.encode(buffer);
 
-      this.authentication_code = URLSafeBase64.encode(buffer);
-
-      console.log("Authentication Code Is: " + this.authentication_code);
-      return this.authentication_code;
-
+        return this.authentication_code;
+      })
+    .catch(err => {
+      throw new assert.AssertionError({message: "FAIL"});
     });
   }
 
   generateSubscriptionID() {
-    return new Promise((resolve, reject) => {
-      this.createAppeal(config.appeal_post_data)
-        .then((result, error) => {
-          return this.createEvent(result.id, config.appeal_event_post_data);
-        }).then((result, error) => {
-        return this.createSubscription(result.id, config.subscriptions_post_data);
-      }).then((result, error) => {
-        resolve({id: result.id});
+    return this.apiHelper.createDefaultAppeal()
+      .then(result => {
+        return this.apiHelper.createAppealReceivedEvent(result.appealId);
       })
-    });
-  }
-
-  createAppeal(appealData) {
-    return request('POST', config.api_base_url + '/appeals')
-      .send(appealData)
-      .then(res => {
-          const header_location = res.headers.location;
-          const location_split = header_location.split("/");
-          const appealId = location_split[2];
-          return {id: appealId};
-        }, err => {
-          throw new assert.AssertionError({message: "Backend API service error on appeal post " + err});
-        }
-      );
-  }
-
-  createEvent(appealID, appealEventData) {
-    return request('POST', config.api_base_url + '/appeals/' + appealID + '/events')
-      .send(appealEventData)
-      .then(res => {
-          console.log("Event Created For Appeal received: " + res.statusCode + " and appeal Id is : " + appealID);
-          return {id: appealID};
-        }, err => {
-          throw new assert.AssertionError({message: "Backend API service failure on appeal received event post " + err});
-        }
-      );
-  }
-
-  createSubscription(appealID, subscriptionsData) {
-    return request('POST', config.api_base_url + '/appeals/' + appealID + '/subscriptions')
-      .send(subscriptionsData)
-      .then(res => {
-          return {id: res.body.subscription.id};
-        },
-        err => {
-          throw new assert.AssertionError({message: "Backend API service failure on subscription creation" + err});
-        }
-      );
+      .then(result => {
+        return this.apiHelper.createDefaultSubscription(result.appealId);
+      });
   }
 
   getMessageAuthenticationCode() {
