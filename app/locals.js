cd880fcbfc26dfce097e9a36a2360a271de338a5
Rob Young <rob@robyoung.digital>
2017-04-10 15:41:23 +0100

    Move locale into res.locals
    Moving the locale into the res.locals object makes it easier to factor
    out parts of the application into libraries.
    
    This change adds the locale to the res.locals object so that it can be
    retrieved from the request context rather than globally. This change
    does not select which locale to use it just makes a locale (at the
    moment just english) available to the request.

--- app/locals.js
+++ app/locals.js
@@ -5,6 +5,7 @@ const locale = require('app/assets/locale/en');
 module.exports = (req, res, next) => {
   res.locals.asset_path = '/public/';
   res.locals.releaseVersion = 'v' + packageJson.version;
+  res.locals.i18n = locale;
   res.locals.cookieText = locale.cookiePolicy.banner.text;
   res.locals.cookieLink = locale.cookiePolicy.banner.link;
 
