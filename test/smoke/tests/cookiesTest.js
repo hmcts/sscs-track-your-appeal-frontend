2557a2855b3eb9c30bc9dc6d14fc3416fd8fb2ea
vreddy <vivek.reddy@hmcts.net>
2017-06-06 15:08:10 +0100

    Refactored e2e tests (#91)
    * * Updated tests to read text from locale language file.
    * * shorten the text in test.

--- test/smoke/tests/cookiesTest.js
+++ test/smoke/tests/cookiesTest.js
@@ -1,25 +1,29 @@
+const cookiePolicyText = require('public/locale/en').cookiePolicy;
+const hearingDetailsText = require('public/locale/en').hearing.details;
+
 Feature('Track your Appeal Cookies Tests');
 
 Scenario('clear cookies and check for cookie message', function*(I, Properties) {
-    let appealId = yield I.retrieveAppealNumberForAppealReceivedAppealCaseID();
-    I.amOnPage('/progress/' + appealId + '/trackyourappeal');
-    I.clearCookie();
-    I.see("GOV.UK uses cookies to make the site simpler. Find out more about cookies");
-    });
+  let appealId = yield I.retrieveAppealNumberForAppealReceivedAppealCaseID();
+  I.amOnPage('/progress/' + appealId + '/trackyourappeal');
+  I.clearCookie();
+  I.see(cookiePolicyText.banner.text);
+  I.see(cookiePolicyText.banner.link);
+});
 
 Scenario('accept cookie and verify cookie', function*(I, Properties) {
   let appealId = yield I.retrieveAppealNumberForAppealReceivedAppealCaseID();
   I.amOnPage('/progress/' + appealId + '/trackyourappeal');
-  I.click('What to expect at your hearing');
-  I.seeCookie('seen_cookie_message');
-
+  I.click(hearingDetailsText.title);
+  I.seeCookie(cookiePolicyText.ourIntroductoryMessage.items[0].name);
 });
 
 Scenario('verify cookie message page', function*(I, Properties) {
   let appealId = yield I.retrieveAppealNumberForAppealReceivedAppealCaseID();
   I.amOnPage('/progress/' + appealId + '/trackyourappeal');
-  I.click('Find out more about cookies');
+  I.click(cookiePolicyText.banner.link);
   I.seeInCurrentUrl('cookiepolicy');
-  I.see("Cookie Policy");
-  I.see("The ‘track your benefit appeal’ website stores small files (known as cookies), on your computer. They are used to improve your experience of using the website.");
+  I.see(cookiePolicyText.cookies.title);
+  I.see(cookiePolicyText.cookies.description);
+
 });
--- test/smoke/tests/manageNotifications.js
+++ test/smoke/tests/manageNotifications.js
@@ -1,29 +1,31 @@
+const emailText = require('public/locale/en').notifications.email;
+
 Feature('Manage Notifications');
 
 Scenario('verify change password ', function*(I, Properties) {
   let authenticationCode = yield I.getMACToken();
   I.amOnPage('/manage-email-notifications/' + authenticationCode);
-  I.see('What do you want to do?');
-  I.checkOption("Change your email address");
+  I.see(emailText.manage.title);
+  I.checkOption(emailText.addressChangeConfirmed.link);
   I.click("Continue");
   I.wait(1);
   I.fillField('email', 'test2.test@hmcts.net');
   I.fillField('email2', 'test2.test@hmcts.net');
   I.click("Continue");
   I.wait(1);
-  I.see("Emails about your ESA benefit appeal will now be sent to");
+  I.see(emailText.addressChangeConfirmed.title);
   I.see("test2.test@hmcts.net");
-
+  I.see(emailText.addressChangeConfirmed.content);
 });
 
 Scenario('stop email subscription  ', function*(I, Properties) {
   let authenticationCode = yield I.getMessageAuthenticationCode();
   I.amOnPage('/manage-email-notifications/' + authenticationCode);
-  I.see('What do you want to do?');
+  I.see(emailText.manage.title);
   I.checkOption("Stop receiving emails");
   I.click("Continue");
-  I.see("You’ll stop receiving email updates and reminders about your ESA benefit appeal.");
+  I.see(emailText.stop.title);
   I.click("Confirm");
-  I.see("You’ve stopped email notifications");
-  I.see("You won’t receive any more emails about your ESA benefit appeal.");
+  I.see(emailText.stopConfirmation.title);
+  I.see(emailText.stopConfirmation.content);
 });
