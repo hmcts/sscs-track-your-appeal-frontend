35156fb182369926a6390527e697411ea7b70906
Rob Young <rob@robyoung.digital>
2017-06-13 09:15:08 +0100

    Remove all trace of HealthService

--- app/assets/javascripts/ServiceLoader.js
+++ app/assets/javascripts/ServiceLoader.js
@@ -1,5 +1,4 @@
 module.exports = {
   AppealService: require('app/services/AppealService'),
-  HealthService: require('app/services/HealthService'),
   TokenService:  require('app/services/TokenService')
 };
--- test/mock/MockServiceLoader.js
+++ test/mock/MockServiceLoader.js
@@ -1,5 +1,4 @@
 module.exports = {
   AppealService: require('test/mock/MockAppealService'),
-  HealthService: require('test/mock/MockHealthService'),
   TokenService:  require('test/mock/MockTokenService')
 };
