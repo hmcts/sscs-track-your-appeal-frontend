198ae694d34ed30a41ec6f9367dff4cbc45dbc27
Paul Gain <paul@javexconsulting.com>
2017-06-14 20:24:44 +0100

    small-tidy-up * Basic cleanup and added some safeguards before making API calls.

--- app/services/TokenService.js
+++ app/services/TokenService.js
@@ -1,23 +1,26 @@
-const request = require('superagent');
 const {tokenAPI} = require('app/config');
+const request = require('superagent');
+const HttpStatus = require('http-status-codes');
 const logger = require('nodejs-logging').getLogger('TokenService.js');
 
 class TokenService {
 
   static validateToken(req, res, next) {
-    if (req.params.mactoken) {
-      request.get(`${tokenAPI}/${req.params.mactoken}`)
-        .then((result) => {
-          res.locals.token = result.body.token;
-          logger.info(`GET /tokens/${req.params.mactoken} 200`);
-          next();
-      }).catch((error) => {
-        next(error);
-      });
-    } else {
-      next();
+    if(!req.params.mactoken) {
+      next(new Error(`Unable to make API call to ${tokenAPI}/${req.params.mactoken}`));
+      return;
     }
+
+    request.get(`${tokenAPI}/${req.params.mactoken}`)
+      .then((result) => {
+        res.locals.token = result.body.token;
+        logger.info(`GET /tokens/${req.params.mactoken} ${HttpStatus.OK}`);
+        next();
+    }).catch((error) => {
+      next(error);
+    });
   }
+
 }
 
 module.exports = TokenService;
--- test/unit/core/NunjucksUtilsTest.js
+++ test/unit/core/NunjucksUtilsTest.js
@@ -19,21 +19,26 @@ describe('NunjucksUtils.js', () => {
     httpServer.close();
   });
 
-  let monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-  ];
-
   // Date & time in UTC.
-  const nowUTC = moment.utc();
-  const utcDateTimeStr = nowUTC.format(dateFormat.utc);
+  const utc = moment.utc();
+  const utcDateTimeStr = utc.format(dateFormat.utc);
 
   // Date & time in local time.
-  nowUTC.local();
-  const date = nowUTC.date();
-  const month = monthNames[nowUTC.month()];
-  const year = nowUTC.year();
-  const localHours = nowUTC.hours();
-  let localMinutes = nowUTC.minutes();
+  const local = utc.local();
+
+  // Extract date, month & year.
+  const localDate = local.date();
+  const localMonth = moment.months()[local.month()];
+  const localYear = local.year();
+
+  // Format hours.
+  let localHours = local.hours();
+  if(localHours < 10) {
+    localHours = `0${localHours}`;
+  }
+
+  // Format minutes.
+  let localMinutes = local.minutes();
   if(localMinutes < 10) {
     localMinutes = `0${localMinutes}`;
   }
@@ -48,21 +53,21 @@ describe('NunjucksUtils.js', () => {
     describe('filters', () => {
 
       it('should take a date defined in UTC and covert it to a local date', () => {
-        let localDate = NunjucksUtils.renderString('{{date|formatDate}}', { date: utcDateTimeStr });
-        expect(localDate).to.equal(`${date} ${month} ${year}`);
+        let localDateTimeStr = NunjucksUtils.renderString('{{date|formatDate}}', { date: utcDateTimeStr });
+        expect(localDateTimeStr).to.equal(`${localDate} ${localMonth} ${localYear}`);
       });
 
       it('should take a time defined in UTC and covert it to local time', () => {
-        let localTime = NunjucksUtils.renderString('{{time|formatTime}}', { time: utcDateTimeStr });
-        expect(localTime).to.equal(`${localHours}:${localMinutes}`);
+        let localDateTimeStr = NunjucksUtils.renderString('{{time|formatTime}}', { time: utcDateTimeStr });
+        expect(localDateTimeStr).to.equal(`${localHours}:${localMinutes}`);
       });
 
       it('should convert a JavaScript object into a JSON string', () => {
-        const obj = {
+        const credentials = {
           username: 'harrypotter',
           password: '123'
         };
-        let stringifiedObj = NunjucksUtils.renderString('{{obj|json}}', {obj: obj});
+        let stringifiedObj = NunjucksUtils.renderString('{{obj|json}}', {obj: credentials});
         expect(stringifiedObj).to.eq('{\n  &quot;username&quot;: &quot;harrypotter&quot;,\n  &quot;password&quot;: &quot;123&quot;\n}');
       });
 
