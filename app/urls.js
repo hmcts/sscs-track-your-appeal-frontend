f80f08ae6727e7fbd03ed8ec42dab2998f463d6f
Paul Gain <paul@javexconsulting.com>
2017-06-26 14:48:56 +0100

    SSCS-1473-feedback-link * Updated URL.

--- app/urls.js
+++ app/urls.js
@@ -2,5 +2,5 @@ module.exports = {
   aboutcookies: 'https://www.aboutcookies.org',
   searches: 'https://insidegovuk.blog.gov.uk/2015/03/26/new-tool-to-see-trending-searches',
   goolgeOptOut: 'https://tools.google.com/dlpage/gaoptout',
-  feedback: 'http://www.smartsurvey.co.uk/s/feedbacksurvey1'
+  feedback: 'http://www.smartsurvey.co.uk/s/VVDB3'
 };
