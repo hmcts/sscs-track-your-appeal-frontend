f5994919fad5ed375ad03e7a6d3b910d3ddeb10d
Paul Gain <paul.gain@hmcts.net>
2017-07-18 13:25:34 +0100

    SSCS-1645-update-hearing-event-content * Updating hearing event content.

--- test/mock/data/index.js
+++ test/mock/data/index.js
@@ -1,29 +1,15 @@
-const adjourned = require('./adjourned.json');
-const appealReceived = require('./appealReceived.json');
-const closed = require('./closed.json');
-const dormant = require('./dormant.json');
-const dwpRespond = require('./dwpRespond.json');
-const dwpRespondOverdue = require('./dwpRespondOverdue.json');
-const hearing = require('./hearing.json');
-const hearingBooked = require('./hearingBooked.json');
-const lapsedRevised = require('./lapsedRevised.json');
-const newHearingBooked = require('./newHearingBooked.json');
-const pastHearingBooked = require('./pastHearingBooked.json');
-const postponed = require('./postponed.json');
-const withdrawn = require('./withdrawn.json');
-
 module.exports = {
-  md001: adjourned,
-  md002: appealReceived,
-  md003: closed,
-  md004: dormant,
-  md005: dwpRespond,
-  md006: dwpRespondOverdue,
-  md007: hearing,
-  md008: hearingBooked,
-  md009: lapsedRevised,
-  md010: newHearingBooked,
-  md011: pastHearingBooked,
-  md012: postponed,
-  md013: withdrawn
+  md001: require('./adjourned.json'),
+  md002: require('./appealReceived.json'),
+  md003: require('./closed.json'),
+  md004: require('./dormant.json'),
+  md005: require('./dwpRespond.json'),
+  md006: require('./dwpRespondOverdue.json'),
+  md007: require('./hearing.json'),
+  md008: require('./hearingBooked.json'),
+  md009: require('./lapsedRevised.json'),
+  md010: require('./newHearingBooked.json'),
+  md011: require('./pastHearingBooked.json'),
+  md012: require('./postponed.json'),
+  md013: require('./withdrawn.json')
 };
