d174113ebbcd3ce8dcf711ba5cbafd2e59d52074
Paul Gain <paul.gain@hmcts.net>
2017-07-20 17:55:02 +0100

    SSCS-1541-add-exit-feedback-survey-link * Added a survey link so the stopped email notifications html page.

--- app/urls.js
+++ app/urls.js
@@ -2,5 +2,6 @@ module.exports = {
   aboutcookies: 'https://www.aboutcookies.org',
   searches: 'https://insidegovuk.blog.gov.uk/2015/03/26/new-tool-to-see-trending-searches',
   goolgeOptOut: 'https://tools.google.com/dlpage/gaoptout',
-  feedback: 'http://www.smartsurvey.co.uk/s/VVDB3'
+  feedback: 'http://www.smartsurvey.co.uk/s/VVDB3',
+  exitFeedbackSurvey: 'http://www.smartsurvey.co.uk/s/appealabenefitdecisionexitsurvey'
 };
