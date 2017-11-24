12ac8f37ab4e4f4cc43f7b07671b639ca1452cc1
Paul Gain <paul.gain@hmcts.net>
2017-11-24 13:05:16 +0000

    Fix for incorrect benefit type being displayed on /abouthearing

--- app/locals.js
+++ app/locals.js
@@ -2,12 +2,13 @@ const types = require('app/core/notifications/types');
 const packageJson = require('../package.json');
 const locale = require('app/assets/locale/en');
 const urls = require('app/urls');
+const { cloneDeep } = require('lodash');
 
 module.exports = (req, res, next) => {
   res.locals.asset_path = '/public/';
   res.locals.releaseVersion = 'v' + packageJson.version;
-  res.locals.i18n = locale;
-  res.locals.cookies    = locale.cookiePolicy.footer.cookies;
+  res.locals.i18n = cloneDeep(locale);
+  res.locals.cookies = locale.cookiePolicy.footer.cookies;
   res.locals.cookieText = locale.cookiePolicy.banner.text;
   res.locals.cookieLink = locale.cookiePolicy.banner.link;
   res.locals.types = types;
