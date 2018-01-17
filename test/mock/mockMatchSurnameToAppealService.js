67afdc9abd31a4157e2079d59b2e87c22e12af5b
bendiggle <ben.diggle5@gmail.com>
2018-01-17 19:03:53 +0000

    finish mockservice and add surname to index to mock data

--- test/mock/mockMatchSurnameToAppealService.js
+++ test/mock/mockMatchSurnameToAppealService.js
@@ -1,17 +1,22 @@
+const mockedData = require('test/mock/data/index');
+const { map, find, includes } = require('lodash');
+
 const matchSurnameToAppeal = (req, res) => {
 
-  console.log('meow');
+  const flattenData = map(mockedData, 'appeal');
+  const mockedDataSurname = map(flattenData, 'surname');
   const mactoken = req.params.mactoken;
   const surname = req.body.surname;
-  const appealSurname = 'meow';
 
-  if (surname === appealSurname) {
-    const id = 'md001';
+  if (includes(mockedDataSurname, surname)) {
+    const appeal = find(flattenData, { surname });
+    const id = appeal.appealNumber;
     res.redirect(`/progress/${id}/trackyourappeal`);
   } else {
     const fields = {
       error: true,
       surname: {
+        value: surname,
         error: true,
         errorMessage: res.locals.i18n.validateSurname.surname.errors.noMatch,
         errorHeading: res.locals.i18n.validateSurname.surname.errors.noMatch
