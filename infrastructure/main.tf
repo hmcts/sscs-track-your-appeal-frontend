01292bfe82d7d4705059fd3fb8897b6e4df7eb5b
Sathish Jeganathan <Satish.Jaganathan@HMCTS.NET>
2018-01-30 10:13:07 +0000

    SSCS-2559 Added infrastructure code and parameterized Jenkinsfile

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,9 +1,9 @@
 module "tract-your-appeal-frontend" {
-  source   = "git@github.com:contino/moj-module-webapp?ref=0.0.78"
+  source   = "git@github.com:contino/moj-module-webapp?ref=master"
   product  = "${var.product}-frontend"
   location = "${var.location}"
   env      = "${var.env}"
-  asename  = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  ilbIp    = "${var.ilbIp}"
 
   app_settings = {
     SSCS_API_URL       = "http://sscs-tribunals-api-${var.env}.service.${data.terraform_remote_state.core_apps_compute.ase_name[0]}.internal"
