a828a1307b0f1f4ce9cfd77bc47e8f5fdd9ab67c
bendiggle <ben.diggle5@gmail.com>
2018-02-14 16:52:54 +0000

    add matchsurname service to js index

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,4 +1,5 @@
 const { getAppeal, changeEmailAddress, stopReceivingEmails } = require('app/services/appealService');
 const { validateToken } = require('app/services/tokenService');
+const { matchSurnameToAppeal } = require('app/services/matchSurnameToAppeal');
 
-module.exports = { getAppeal, changeEmailAddress, stopReceivingEmails, validateToken };
+module.exports = { getAppeal, changeEmailAddress, stopReceivingEmails, validateToken, matchSurnameToAppeal };
