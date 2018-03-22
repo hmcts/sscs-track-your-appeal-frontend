834b5c6352e67675cd039d8d279de7a12686a332
Paul Gain <paul.gain@javexconsulting.com>
2018-03-22 16:05:18 +0000

    Temporary log to ensure the appeal is in the correct state

--- app/services/appealService.js
+++ app/services/appealService.js
@@ -4,6 +4,7 @@ const HttpStatus = require('http-status-codes');
 const request = require('superagent');
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('AppealService.js');
+const { cloneDeep } = require('lodash');
 
 const getAppeal = (req, res, next) => {
 
@@ -15,7 +16,7 @@ const getAppeal = (req, res, next) => {
   return request.get(`${apiUrl}/appeals/${req.params.id}`)
     .then((result) => {
       const appeal = result.body.appeal;
-      logger.info(appeal);
+      logger.info(cloneDeep(appeal));
       appeal.evidenceReceived = false;
       appeal.latestEvents = appeal.latestEvents || [];
       appeal.historicalEvents = appeal.historicalEvents || [];
