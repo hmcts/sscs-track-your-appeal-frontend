f5db9cb0867d10a8a8dbca5a4ec27fa88ad94740
istredd <labnob@gmail.com>
2018-04-11 15:51:48 +0100

    Fix to net vnet world

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,6 +14,13 @@ data "vault_generic_secret" "hpkp_tya_sha_2" {
   path = "secret/${var.infrastructure_env}/sscs/hpkp_tya_sha_2"
 }
 
+locals {
+  aseName = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+
+  localApiUrl = "http://sscs-tribunals-api-${var.env}.service.${local.aseName}.internal"
+  ApiUrl = "${var.env == "preview" ? "http://sscs-tribunals-api-aat.service.core-compute-aat.internal" : local.localApiUrl}"
+}
+
 module "tract-your-appeal-frontend" {
   source               = "git@github.com:contino/moj-module-webapp?ref=master"
   product              = "${var.product}-frontend"
@@ -26,7 +33,7 @@ module "tract-your-appeal-frontend" {
 
 
   app_settings = {
-    SSCS_API_URL                 = "http://sscs-tribunals-api-${var.env}.service.${data.terraform_remote_state.core_apps_compute.ase_name[0]}.internal"
+    SSCS_API_URL                 = "${locals.ApiUrl}"
     WEBSITE_NODE_DEFAULT_VERSION = "8.9.4"
     NODE_ENV                     = "${var.infrastructure_env}"
     COOKIE_SECRET                = "${data.vault_generic_secret.cookiesecret.data["value"]}"
