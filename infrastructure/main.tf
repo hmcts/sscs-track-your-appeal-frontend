be8d4b7fe31c31efd99ed170dfb1df1594c7edc4
istredd <labno.b@gmail.com>
2018-04-26 15:41:52 +0100

    Disable https only on preview

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -30,7 +30,7 @@ module "tya-frontend" {
   is_frontend          = "${var.env != "preview" ? 1: 0}"
   subscription         = "${var.subscription}"
   additional_host_name = "${var.env != "preview" ? var.additional_hostname : "null"}"
-  https_only           = "true"
+  https_only           = "${var.env != "preview" ? "true" : "false"}"
 
   app_settings = {
     SSCS_API_URL                 = "${local.ApiUrl}"
