37e414f72ea57953697ce1bab60f26772b0f2649
vianvi <30834324+vianvi@users.noreply.github.com>
2018-07-19 10:11:25 +0100

    remove health check from e2e tests

--- test/e2e/smoke/health.test.js
+++
@@ -1,8 +0,0 @@
-const paths = require('paths');
-
-Feature('Health');
-
-Scenario('The API is up and responding to requests to /health @smoke', I => {
-  I.amOnPage(paths.health);
-  I.see('"status":"UP"');
-});
