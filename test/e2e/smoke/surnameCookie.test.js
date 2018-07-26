afc9d619959e7da003a2201d0d23570e476e4d90
david.crespo <david.crespo@hmcts.net>
2018-07-26 09:14:38 +0200

    Add same wait times that for the smoke test that passes fine

--- test/e2e/smoke/surnameCookie.test.js
+++ test/e2e/smoke/surnameCookie.test.js
@@ -1,7 +1,7 @@
 const { appeal } = require('test/mock/data/smokeTest');
 const paths = require('paths');
 
-const waitTime = 20;
+const waitTime = 10;
 
 Feature('Surname validation cookie');
 
@@ -9,6 +9,8 @@ Scenario('User enters a valid surname and leaves a cookie @smoke', I => {
   I.clearCookie();
   I.amOnPage(`${paths.tya.validateSurname}/${appeal.appealNumber}${paths.tya.trackYourAppeal}`);
   I.enterSurnameAndSubmit('Test');
+  I.wait(3); // eslint-disable-line
+  I.seeCurrentUrlEquals(`${paths.tya.trackYourAppeal}/${appeal.appealNumber}`);
   I.waitForElement('#surname', waitTime);
   I.seeCookie('tya-surname-appeal-validated');
 });
