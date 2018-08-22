1cd357acda4d10fae1f4ca84f9c68954b86d9074
david.crespo <david.crespo@hmcts.net>
2018-08-22 16:51:21 +0100

    SSCS-3780 Update config to use Azure Vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,20 +2,30 @@ provider "vault" {
   address = "https://vault.reform.hmcts.net:6200"
 }
 
-data "vault_generic_secret" "cookiesecret" {
-  path = "secret/${var.infrastructure_env}/sscs/tyacookiesecret"
+data "azurerm_key_vault" "sscs_key_vault" {
+  name = "${local.vaultName}"
+  resource_group_name = "${local.vaultName}"
 }
 
-data "vault_generic_secret" "hpkp_tya_sha_1" {
-  path = "secret/${var.infrastructure_env}/sscs/hpkp_tya_sha_1"
+data "azurerm_key_vault_secret" "cookiesecret" {
+  name = "tyacookiesecret"
+  vault_uri = "${data.azurerm_key_vault.sscs_key_vault.vault_uri}" 
 }
 
-data "vault_generic_secret" "hpkp_tya_sha_2" {
-  path = "secret/${var.infrastructure_env}/sscs/hpkp_tya_sha_2"
+data "azurerm_key_vault_secret" "hpkp-tya-sha-1" {
+  name = "hpkp-tya-sha-1"
+  vault_uri = "${data.azurerm_key_vault.sscs_key_vault.vault_uri}" 
+}
+data "azurerm_key_vault_secret" "hpkp-tya-sha-2" {
+  name = "hpkp-tya-sha-2"
+  vault_uri = "${data.azurerm_key_vault.sscs_key_vault.vault_uri}" 
 }
 
 locals {
   aseName = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  previewVaultName = "${var.raw_product}-aat"
+  nonPreviewVaultName = "${var.raw_product}-${var.env}"
+  vaultName = "${(var.env == "preview" || var.env == "spreview") ? local.previewVaultName : local.nonPreviewVaultName}"
 
   localApiUrl = "http://sscs-tribunals-api-${var.env}.service.${local.aseName}.internal"
   ApiUrl = "${var.env == "preview" ? "http://sscs-tribunals-api-aat.service.core-compute-aat.internal" : local.localApiUrl}"
@@ -37,9 +47,9 @@ module "tya-frontend" {
     SSCS_API_URL                 = "${local.ApiUrl}"
     WEBSITE_NODE_DEFAULT_VERSION = "8.9.4"
     NODE_ENV                     = "${var.infrastructure_env}"
-    COOKIE_SECRET                = "${data.vault_generic_secret.cookiesecret.data["value"]}"
-    HPKP_SHA256                  = "${data.vault_generic_secret.hpkp_tya_sha_1.data["value"]}"
-    HPKP_SHA256_BACKUP           = "${data.vault_generic_secret.hpkp_tya_sha_2.data["value"]}"
+    COOKIE_SECRET                = "${data.azurerm_key_vault_secret.cookiesecret.value}"
+    HPKP_SHA256                  = "${data.azurerm_key_vault_secret.hpkp-tya-sha-1.value}"
+    HPKP_SHA256_BACKUP           = "${data.azurerm_key_vault_secret.hpkp-tya-sha-2.value}"
   }
 }
 
