2cadce007ccc3b84216d31202d020a47564cf371
Chris Moreton <chris@netsensia.com>
2018-10-04 15:47:44 +0100

    Remove output var from TF config

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -2,10 +2,6 @@ output "vaultUri" {
   value = "${data.azurerm_key_vault.sscs_key_vault.vault_uri}"
 }
 
-output "vaultName" {
-  value = "${local.azureVaultName}"
-}
-
 output "microserviceName" {
   value = "${var.component}"
 }
