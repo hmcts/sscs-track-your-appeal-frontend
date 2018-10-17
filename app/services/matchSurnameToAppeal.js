1112febed289dbc32088c3c1d9066df185d6abc4
Paul VM <paul.vm@fullstackwebpro.co.uk>
2018-10-17 09:59:13 +0100

    SSCS-4068 Add log message upon surname verification

--- app/services/matchSurnameToAppeal.js
+++ app/services/matchSurnameToAppeal.js
@@ -3,6 +3,9 @@ const apiUrl = require('config').get('api.url');
 const appInsights = require('app-insights');
 const HttpStatus = require('http-status-codes');
 const { tya } = require('paths');
+const { Logger } = require('@hmcts/nodejs-logging');
+
+const logger = Logger.getLogger('matchSurnameToAppeal.js');
 
 const matchSurnameToAppeal = (req, res, next) => {
   const id = req.params.id;
@@ -10,10 +13,15 @@ const matchSurnameToAppeal = (req, res, next) => {
   const surname = req.body.surname;
 
   return request.get(`${apiUrl}/appeals/${id}/surname/${surname}`)
-    .then(() => {
+    .then(result => {
       req.session[id] = true;
       const pageExists = Object.values(tya).includes(`/${originalPage}`);
       const page = pageExists ? `/${originalPage}` : tya.trackYourAppeal;
+      if (surname !== null && typeof surname !== 'undefined') {
+        const caseId = result.body.caseId;
+        const surnameRedacted = surname.substr(0, 1);
+        logger.info(`POST /appeals/${id}/surname/${surnameRedacted}[REDACTED] ${HttpStatus.OK} for caseId ${caseId}`);
+      }
       res.redirect(`${page}/${id}`);
     })
     .catch(error => {
