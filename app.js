219e7ed4069532358a1a1ebc92f50791da55ce06
Chris Moreton <christopher.moreton@hmcts.net>
2019-04-23 14:41:45 +0100

    Remove early app insights enable

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-require('app-insights').enable();
 const { Express } = require('@hmcts/nodejs-logging');
 const { tyaNunjucks, filters } = require('app/core/tyaNunjucks');
 const health = require('app/services/health');
