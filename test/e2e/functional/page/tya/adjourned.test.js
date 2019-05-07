6ea1b47cc9366f166eb96c42f6645b6fbd8e3516
srochani <sunil.rochani@hotmail.com>
2019-05-07 11:00:00 +0100

    SSCS-5324, SSCS-5328, SSCS-5331, SSCS-5334, SSCS-5341, SSCS-5345 WebPage changes

--- test/e2e/functional/page/tya/adjourned.test.js
+++ test/e2e/functional/page/tya/adjourned.test.js
@@ -2,9 +2,6 @@ const { env } = require('test/e2e/helpers/nunjucksHelper');
 const { appeal } = require('test/mock/data/oral/adjourned');
 const { common, status } = require('public/locale/en');
 
-const latestEvent = appeal.latestEvents[0];
-const date = { adjournedLetterReceivedByDate: latestEvent.adjournedLetterReceivedByDate };
-
 Feature('TYA - Adjourned');
 
 Before(I => {
