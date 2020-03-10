e727873c7d6982dafe065c05d2e6dc0f62d9c639
jackmaloney <jack.maloney@transformuk.com>
2020-03-10 12:13:18 +0000

    Remove infrastructure folder

--- infrastructure/main.tf
+++
@@ -1,8 +0,0 @@
-data "azurerm_key_vault" "sscs_key_vault" {
-  name                = "${local.vaultName}"
-  resource_group_name = "${local.vaultName}"
-}
-
-locals {
-  vaultName           = "${var.raw_product}-${var.env}"
-}
